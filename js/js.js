f6df84f7ead008b8728c6070568a193ca40f296d
hahaveryfun <hahaveryfun@gmail.com>
2015-03-08 22:30:06 -0400

    Project Card Navigation v1
    Project cards are now traversable, click on the card to view cards
    behind it.

--- js/js.js
+++ js/js.js
@@ -11,38 +11,37 @@ function navClick()
     if(navOut())
     {
       //return to normal
-      $(this).attr("src", "images/icons/list.png")
+      $(this).attr("src", "../images/icons/list.png")
       $("#nav-wrapper").animate
       (
         {width:"50px"}
       );
+
       shrink();
+      
       $(".nav-item").toggleClass("nav-item-extended");
     }
     else
     {
       //extend out
-      $(this).attr("src", "images/icons/arrow_left.png")
+      $(this).attr("src", "../images/icons/arrow_left.png")
       $("#nav-wrapper").animate
       (
         {width:"15%"}
       );
+
       spread();
+      
       $(".nav-item").toggleClass("nav-item-extended");
     }
   });
-}
-
-function navHover()
-{
-  //change to a hover inverse color effect
 }
 
 function spread()
 {
   $("#content-wrapper").animate
   ({
-    left: ($(document).width() * .15)-50
+    marginLeft: ($(document).width() * .15)-50
   });
 
   //rotation for each spreadable element
@@ -77,11 +76,10 @@ function shrink()
 {
   $("#content-wrapper").animate
   ({
-    left:0
+    marginLeft:0
   });
   $.each($(".spreadable"),function()
   {
-    var currentrotation = $(this).css("transform");
     var element = $(this);
     $({deg: $(this).data("rotation")}).animate({deg: 0}, 
     {
@@ -98,32 +96,117 @@ function shrink()
   $("#shadowbox").fadeOut(400);
 }
 
-function layOutNames()
+function positionCard(_o, _lower, _upper, _animate)
 {
-  //helper function to create effect on the name
-  var easing = "easeOutBounce";
-  var time = 1000;
-  $('#william').hide();
-  $('#eric').hide();
-  $('#dunkerley').hide();
-  $('#william').slideDown(1200, easing, function() 
+  //_o is passed $(this)
+  var randomleft = Math.floor(Math.random() * (_upper - _lower)) + _lower;
+  var randomtop = Math.floor(Math.random() * (_upper - _lower)) + _lower;
+  if (_animate)
   {
-    //recursive for each navitem
-  });
-  $('#eric').slideDown(1000, easing, function() 
+    var currentleft = 100*parseInt(_o.css("left"))/parseInt(_o.parent().css("width"));
+    var currenttop = 100*parseInt(_o.css("top"))/parseInt(_o.parent().css("height"));
+    //animate from their current to the target randomleft and randomtop
+    $({leftcurr: currentleft}).animate({leftcurr: randomleft}, 
     {
-      //recursive for each navitem
-
-    });
-        $('#dunkerley').slideDown(800, easing, function() 
+      duration: 500,
+      step: function(step) 
       {
-        
+        _o.css
+        ({
+          left: step + '%'
+        });
+      }
+    });
+    $({topcurr: currenttop}).animate({topcurr: randomtop}, 
+    {
+      duration: 500,
+      step: function(step) 
+      {
+        _o.css
+        ({
+          top: step + '%'
+        });
+      }
+    });
+  }
+  else
+  {
+    _o.css(
+    {
+      left:randomleft+"%",
+      top:randomtop+"%"
+    })
+  }
+
+}
+
+function projectClick()
+{
+  $(".project-shadowbox").hide();
+  //sets up the hidden cards first
+
+  $.each($(".project-card"), function()
+  {
+    $(this).hide();
+    positionCard($(this), 40, 60, false);
+  });
+  //fades away intro project text, cards fadeIn, then cluster
+  $(".project-intro").click(function()
+  {
+    $.each($(".project-card"), function()
+    {
+      $(this).fadeIn(750);
+      positionCard($(this), 47, 53, true);
+    });
+    $(this).toggleClass("project-intro-hidden");
+    $(".project-shadowbox").fadeIn(500);
+  });
+
+  $("body").on("click", ".project-card", function()
+  {
+    swapCard($(".project-card:last"));
+  });
+}
+
+function swapCard(_o)
+{
+  //takes card _o and shifts it to the back of the line
+  var element = _o;
+  $(".project-shadowbox").fadeOut(300, function(){$(this).remove()});
+  $({swapmargin: 0}).animate({swapmargin: 500}, 
+  {
+    duration: 500,
+    step: function(step) 
+    {
+      _o.css
+      ({
+        marginLeft: step + 'px'
       });
+    },
+    complete: function()
+    {
+      //card needs to go back
+      _o.remove();
+      $("#cards-wrapper").prepend(element);
+      $("<div class='project-shadowbox'></div>").insertBefore(".project-card:last")
+      $(".project-shadowbox").hide().fadeIn(500);
+      $({swapmargin: 500}).animate({swapmargin: 0}, 
+      {
+        duration: 500,
+        step: function(step) 
+        {
+          element.css
+          ({
+            marginLeft: step + 'px'
+          });
+        }
+      });
+    }
+  });
 }
 
 $(document).ready(function()
 {
-  // navHover();
-  // layOutNames();
   navClick();
+  projectClick();
 });
